5db9fecc683fad8ae0db8367fe426921f9f92bd8
YvesCesar <yvesamorim73@gmail.com>
2025-12-31 13:25:09 -0400

    Move Doom bundle to JS folder

--- js/load-game.js
+++ js/load-game.js
@@ -1,4 +1,4 @@
 Dos(document.getElementById("doom"), {
-    url: OC.filePath('doom_nextcloud', '', 'bundle/doom.jsdos'),
+    url: OC.filePath('doom_nextcloud', 'js', 'doom.jsdos'),
     pathPrefix: OC.filePath('doom_nextcloud', 'js', 'emulators/'),
 });
